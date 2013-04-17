0e32c2d689e8373b89ca493867a18b7feb33c2f3
Clément Rivaille <itooh@clement-Ubuntu.(none)>
2013-04-17 15:29:30 +0200

    Suite à la demande de Gael, modification de l'interface de la commande de virage (l'angle n'est plus nécessaire) MODIFIEZ VOS CLASSES EN CONSCÉQUENCES

--- src/kercar/comAPI/CMDTurnMessage.java
+++ src/kercar/comAPI/CMDTurnMessage.java
@@ -2,34 +2,22 @@ package kercar.comAPI;
 
 public class CMDTurnMessage extends Message implements ICMDTurnMessage {
 	
-	public static final int INDEX_ANGLE = 0;
-	public static final int INDEX_RIGHT = 1;
+	public static final int INDEX_RIGHT = 0;
 	
 	/**
 	 * Création d'une commande de virage
 	 * @param angle : Angle du virage
 	 * @param right : Vrai pour un virage à droite, faux pour à gauche
 	 */
-	public CMDTurnMessage(int angle, boolean right) {
+	public CMDTurnMessage(boolean right) {
 		super(Message.CMD_TURN);
 		
-		this.params.add(INDEX_ANGLE, Integer.toString(angle));
 		this.params.add(INDEX_RIGHT, Boolean.toString(right));
 	}
 	
 	public CMDTurnMessage(Message m){
 		super(Message.CMD_TURN);
 		this.params = m.getParams();
-	}
-
-	@Override
-	public int getAngle() {
-		return Integer.parseInt(this.params.get(INDEX_ANGLE));
-	}
-
-	@Override
-	public void setAngle(int angle) {
-		this.params.set(INDEX_ANGLE, Integer.toString(angle));
 	}
 
 	@Override
--- src/kercar/comAPI/ICMDTurnMessage.java
+++ src/kercar/comAPI/ICMDTurnMessage.java
@@ -2,9 +2,6 @@ package kercar.comAPI;
 
 public interface ICMDTurnMessage {
 	
-	public int getAngle();
-	public void setAngle(int angle);
-	
 	public boolean isTurningRight();
 	public void turnRight();
 	public void turnLeft();
