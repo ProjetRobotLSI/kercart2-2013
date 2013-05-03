6995ed7871361c028e76ea710a6e8a2472f34505
Brice Pepin <brice.pepin@gmail.com>
2013-05-03 16:16:50 +0200

    Ajout liste des missions

--- KercarAndroid/src/com/kercar/MenuCreation.java
+++ KercarAndroid/src/com/kercar/MenuCreation.java
@@ -1,49 +1,68 @@
 package com.kercar;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable;
 
+import BaseDeDonnees.Mission;
+import Client.ClientMissions;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 public class MenuCreation extends Activity{
-
-		private ListView liste= null;
-		private Button btnCreation= null;
+	private ListView listeMissions;
+	private Button btnCreation;
 	
-		public void onCreate(Bundle savedInstanceState) {
-		    super.onCreate(savedInstanceState);
-		    
-		  /**ContentView*/
-		    setContentView(R.layout.menu_creation);
-		    
-		  /**Initialisation des attributs*/
-		    liste = (ListView) findViewById(R.id.lstListeMissions);
-		    btnCreation = (Button)findViewById(R.id.send);
-		    
-		  /**Traitements sur lstListeMissions*/
-		    List<String> exemple = new ArrayList<String>();
-		    exemple.add("Item 1");
-		    exemple.add("Item 2");
-		    exemple.add("Item 3");
-		         
-		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, exemple);
-		    liste.setAdapter(adapter);
-		    
-		  /**Traitement sur btnCreation*/
-		    //Listener
-		    btnCreation.setOnClickListener(new OnClickListener(){
-				@Override
-				public void onClick(View v) {
-					Intent intent = new Intent(MenuCreation.this, CreationForm.class);
-					startActivity(intent);
-				}
-			});
-		  }
+	private ClientMissions gestionMissions;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu_creation);
+
+		//Initialisation des attributs d'affichage
+		listeMissions = (ListView) findViewById(R.id.lstListeMissions);
+		btnCreation = (Button)findViewById(R.id.send);
+		
+		// récupération des missions dans la base de données
+		gestionMissions = new ClientMissions(getApplicationContext());
+
+		// Affichage des missions créées		
+		ArrayAdapter<Mission> adapter = new ArrayAdapter<Mission>(this, android.R.layout.simple_list_item_1, gestionMissions.getListeMissions().getListe());
+		listeMissions.setAdapter(adapter);
+
+		/**Listener de création d'une nouvelle mission*/
+		btnCreation.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MenuCreation.this, CreationForm.class);
+				startActivity(intent);
+			}
+		});
+		
+		/**
+		 * Listener d'édition des missions déjà créées
+		 */
+		listeMissions.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Bundle bundle = new Bundle();
+				
+				//Récupération de la mission selectionnée
+				Mission mission = gestionMissions.getListeMissions().getListe().get(position);
+				bundle.putSerializable("mission", (Serializable) mission);
+				
+				//Passage à la page d'édition (qui est la même que la création)
+				Intent intent = new Intent(MenuCreation.this, CreationForm.class);
+				intent.putExtras(bundle);
+				
+				startActivity(intent);
+			}
+		});
+	}
 }
